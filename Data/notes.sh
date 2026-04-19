9f3c33e0fa8e81830e89130ea4c0c1a4ebc3b7ca
omargablx01 <o.ashraf.gabl@gmail.com>
2026-04-19 22:14:32 +0200

    Last 23

--- Data/notes.sh
+++ Data/notes.sh
@@ -706,3 +706,369 @@ if [ -e $filename ] ; then
 else
     echo "${filename} Not Found,,"
 fi
+#? ------------------------------------- 16 -------------------------------------
+# Video 16 => https://www.youtube.com/watch?v=rv4sXWeJa_4&list=PLBdyyeW_Z41DykncH9zzMk8T7Rm5UlZXd&index=16
+
+#                          >>>>>>>>> Regular Expression part 1 <<<<<<<<<<
+#? ------------------------------------- 16 -------------------------------------
+#! Regular Expression هنا عامل زي ال python بالظبط مفيش اي اختلاف
+#* \d => One digit > رقم واحد
+#* \D => One non-digit > اي حاجه غير الارقام
+#* \s => One whitespace > بيعلم علي المسطره " "
+#* \S => One non-whitespace > بيعلم علي اي حاجه غير المسطره 
+#* \w => One word character > بيعلم علي الاحرف والارقام
+#* \W => One non-word character > بيعلم علي اي حاجه غير الاحرف والارقام
+#* \d\s => كدا عايز رقم وراه مسطره
+#* \d\d => كدا عايز رقمين واره بعض وكل ما تزود كل ما تزود رقم
+# *	0 or more
+#! مثال > \w* > صفر او اكثر
+# +	1 or more
+#! مثال > \w+ > حرف واحد او اكثر 
+# ?	0 or 1
+#! مثال > \d? > لو رقم موجود تمام لو مش موجود اكن مفيش حاجه
+# {3}	Exactly 3
+#! مثال > \w{3} > هيدور علي 3 حروف فقط
+# {1,3}	Between 1 and 3
+#! مثال > \w{1,3} > هيدور علي الي بين 1 و 3
+# {2,}	2 or more
+#! مثال > \w{1,} > هيدور علي 2 او اكثر 
+# (,5}	Up to 5
+#! مثال > \w{,5} > من ال 0 حتي ال 5 اخرك 5
+# [0-9] > من 0 الي 9
+# [^0-9] > اعرض كل حاجه معاده من 0 الي 9 
+# [A-Z] > من A الي Z حروف كبيره
+# [^A-Z] > اعرض كل حاجه معاده من A الي Z 
+# [a-z] من a الي z حروف صغيره
+# [^a-z] اعرض كل حاجه معاده من a الي z 
+# [a-z0-9] > كدا الي بين a , z & 0 , 9
+# [^a-z0-9] > كدا معاده الي بين a , z & 0 , 9
+# \s[0-9] > اعرض الي قبله مسطره وبعده حرف واحد فقط من 0,9
+# [sS]ound  >> sound دي | Sound او دي
+#! مثال > \d? > لو رقم موجود تمام لو مش موجود اكن مفيش حاجه
+# ?	0 or 1
+#! مثال > ^\d > لازم يبداء برقم 
+# ^  Start of String
+#! مثال > $\d > لازم ينتهي برقم 
+# $  End of string
+#? (\d-|\d\)|\d>) > بيشوف لو بيبداء برقم و علامه - او رقم وعلامه ) او رقم وعلامه > و
+# |	  Or
+#? علامه ال Escape علشان تخلي يعدل العلامه دي 
+# \	  Escape Special Characters >> \+ = + , \* = * , etc..
+#? (\d{4}) > دا كدا يسمي مجموعه مع بعض 
+# ()  Separate Groups
+#! (.+) > نستخدم ال .+ بعمني اي حاجه وكمان مش عارف العدد بتاعها 
+# .  اي حاجه etc..
+#? ------------------------------------- 17 -------------------------------------
+# Video 17 => https://www.youtube.com/watch?v=Uknv-I7wCXY&list=PLBdyyeW_Z41DykncH9zzMk8T7Rm5UlZXd&index=17
+
+#                          >>>>>>>>> Regular Expression part 2 <<<<<<<<<<
+#? ------------------------------------- 17 -------------------------------------	 
+# https://regexone.com/lesson/introduction_abcs
+# solution > \w+  (OR)  abc 
+# abcdefg
+# abcde
+# abc
+#* ------------------------------ >>
+# https://regexone.com/lesson/letters_and_digits
+# solution > \d+  (OR)  \d{3}
+# abc123xyz
+# define "123"
+# var g = 123;
+#* ------------------------------ >>
+# https://regexone.com/lesson/wildcards_dot
+# solution > ...\.  (OR)  \.  (OR)  .*[^abc1]
+# هنا معنه انه بيبداء ب 3 حاجات وبينتهي ب .
+# .*[^abc1] > استخدام ال .* معنها اي حاجه بستثناء حروف ال abc1
+# match : cat.
+# match : 896.
+# match : ?=+.
+# Skip  : abc1
+#? ------------------------------------- 18 -------------------------------------
+# Video 18 => https://www.youtube.com/watch?v=cBpNghupEeY&list=PLBdyyeW_Z41DykncH9zzMk8T7Rm5UlZXd&index=18
+
+#                          >>>>>>>>> Regular Expression part 3 <<<<<<<<<<
+#? ------------------------------------- 18 -------------------------------------
+# https://regexone.com/lesson/matching_characters
+# solution > [^drp]an  (OR)  [cmf]an  (OR)  ^[cmf]an$
+# [^drp]an >> هنا معنها لا يبداء ب اي حرف من الحروف دي drp وينتهي ب an
+# [cmf]an >> هنا معنها يبداء ب اي حرف من الحروف دي c,m,f وينتهي ب an
+# ^[cmf]an$  >> ^ معنها ان لازم يبداء ب اي حرف من الحروف الثلاثه دول cmf ولازم
+# match	: can	
+# match	: man	
+# match	: fan	
+# skip	: dan
+# skip	: ran
+# skip	: pan
+#* ------------------------------ >>
+# https://regexone.com/lesson/excluding_characters
+# solution > ^[hd]og$  (OR)  [^b]og$
+# [^b]og$ >> علامه ال ^ معنها اي حاجه لا تبداء ب حرف ال d
+# يعني كل الحروف معاده حرف ال d
+# match	 : hog
+# match	 : dog
+# skip	 : bog 
+#* ------------------------------ >>
+# https://regexone.com/lesson/character_ranges
+# solution > [^a-z][a-z]{2}$  (OR)  [A-Z][a-z]{2}$  (OR)  [A-Z]\w{2}$  (OR)  [A-Z]\w+$  (OR)  [A-C][a-p]{2}$
+# solution > [A-C][n-p][a-c]  (OR)  
+# match	 : Ana
+# match	 : Bob
+# match	 : Cpc
+# skip	 : aax
+# skip	 : bby
+# skip	 : ccz
+#? ------------------------------------- 19 -------------------------------------
+# Video 19 => https://www.youtube.com/watch?v=tFB8AY-ShRE&list=PLBdyyeW_Z41DykncH9zzMk8T7Rm5UlZXd&index=19
+
+#                          >>>>>>>>> Regular Expression part 4 <<<<<<<<<<
+#? ------------------------------------- 19 -------------------------------------
+# https://regexone.com/lesson/repeating_characters
+# solution > waz{3,5}up  (OR)  
+# match	:  wazzzzzup
+# match	:  wazzzup
+# skip	:  wazup
+#* ------------------------------ >>
+# https://regexone.com/lesson/kleene_operators
+# solution > [a-c]{3,}  (OR)  a+b*c+  (OR)  a{2,4}b{0,4}c{1,2}
+# match	:  aaaabcc
+# match	:  aabbbbc
+# match	:  aacc
+# skip	:  a
+#* ------------------------------ >>
+# https://regexone.com/lesson/optional_characters
+# solution > .+\? (OR) ^[1-4]{1,2}.+  (OR) [1-4]{1,2}\sfiles?\s\w+\? (OR) \d+ files? \w+\? (OR) \d+ files? found\?
+# match	:  1 file found?
+# match	:  2 files found?
+# match	:  24 files found?
+# skip	:  No files found
+#? ------------------------------------- 20 -------------------------------------
+# Video 20 => https://www.youtube.com/watch?v=I6NMdwAx2Ho&list=PLBdyyeW_Z41DykncH9zzMk8T7Rm5UlZXd&index=20
+
+#                          >>>>>>>>> Regular Expression part 5 <<<<<<<<<<
+#? ------------------------------------- 20 -------------------------------------
+# https://regexone.com/lesson/whitespaces
+# solution > \S\.\s+abc (OR) \d+.\s+[a-c]+ (OR) \d\.\s+abc (OR) 
+# match	 : 1.   abc
+# match	 : 2.	abc
+# match	 : 3.           abc
+# skip	 : 4.abc
+#* ------------------------------ >>
+# https://regexone.com/lesson/line_beginning_end
+# solution > ^Mission: successful$ (OR) ^Mission: \w+ful$ (OR) ^[Ma-z:]+\s\w+$ (OR) 
+# match	:  Mission: successful
+# skip	:  Last Mission: unsuccessful
+# skip	:  Next Mission: successful upon capture of target
+#* ------------------------------ >>
+# https://regexone.com/lesson/capturing_groups
+# solution > ^(file\w+).+ (OR) ^(file\w+)\.pdf (OR) ^(file.+)\.pdf$
+# ^(file\w+).+ >> استخدمن الاقواس بمعني ان دول جروب لوحده بعد كدا كملنا حل عادي 
+# capture	:  file_record_transcript.pdf	    file_record_transcript
+# capture	:  file_07241999.pdf	            file_07241999
+# skip	    :  testfile_fake.pdf.tmp
+#* ------------------------------ >>
+# https://regexone.com/lesson/nested_groups
+# solution > ([A-Za-z]{1,3}\s(\d+)) (OR) (\w+ (\d+)) 
+# capture	 : Jan 1987	   Jan 1987     1987
+# capture	 : May 1969	   May 1969     1969
+# capture	 : Aug 2011	   Aug 2011     2011
+#* ------------------------------ >>
+# https://regexone.com/lesson/more_groups
+# solution > (.*)x(.*) (OR) (\d+)x(\d+) (OR) 
+# capture	:  1280x720	    1280 720
+# capture	:  1920x1600	1920 1600
+# capture	:  1024x768     1024 768
+#* ------------------------------ >>
+# https://regexone.com/lesson/conditionals
+# solution > [A-Z] love (cats|dogs)$ (OR) I love (cats|dogs)
+# match	 : I love cats
+# match	 : I love dogs
+# skip	 : I love logs
+# skip	 : I love cogs
+#* ------------------------------ >> OUT OF Course
+#! دي حاجات انا حليتها من نفسي
+# https://regexone.com/problem/matching_decimal_numbers
+# solution > ^-?\d+(,\d+)*(\.\d+(e\d+)?)?$
+# match	 :  3.14529
+# match	 :  -255.34
+# match	 :  128
+# match	 :  1.9e10
+# match	 :  123,340.00
+# skip	 :  720p
+#* ------------------------------ >> OUT OF Course
+# https://regexone.com/problem/matching_phone_numbers
+# solution > 1? ?\(?(\d{3}?)\)?-?\s?\d+-?\s?\d+ (OR) 1?[\s-]?\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}
+# capture	 :    415-555-1234	    415
+# capture	 :    650-555-2345	    650
+# capture	 :    (416)555-3456	    416
+# capture	 :    202 555 4567	    202
+# capture	 :    4035555678	    403
+# capture	 :    1 416 555 9292    416
+#* ------------------------------ >> OUT OF Course
+# https://regexone.com/problem/matching_emails
+# solution > (\w+\.?\w+)(@|\+)\w+(\.|@)?\w+(\.)?com$ (OR) ^([\w\.]*)(@|\+)\w+(\.|@)?\w+(\.)?com$
+# capture	 :  tom@hogwarts.com	                    tom
+# capture	 :  tom.riddle@hogwarts.com	                tom.riddle
+# capture	 :  tom.riddle+regexone@hogwarts.com	    tom.riddle
+# capture	 :  tom@hogwarts.eu.com	                    tom
+# capture	 :  potter@hogwarts.com	                    potter
+# capture	 :  harry@hogwarts.com	                    harry
+# capture	 :  hermione+regexone@hogwarts.com          hermione
+#* ------------------------------ >> OUT OF Course
+# https://regexone.com/problem/matching_html
+# solution > <([a-z]{1,3})>?\s?.+ (OR) <(\w+).+
+# capture	  :  <a>This is a link</a>	                    a
+# capture	  :  <a href='https://regexone.com'>Link</a>	a
+# capture	  :  <div class='test_style'>Test</div>	        div
+# capture	  :  <div>Hello <span>world</span></div>        div
+#* ------------------------------ >> OUT OF Course
+# https://regexone.com/problem/matching_filenames
+# solution > (\w+)?(img\w+)?\.?(gif|png|jpg)$ (OR) (\w+)?\.(gif|png|jpg)$
+# skip	       :  .bash_profile
+# skip	       :  workspace.doc
+# capture	   :  img0912.jpg	                 img0912 jpg
+# capture	   :  updated_img0912.png	         updated_img0912 png
+# skip	       :  documentation.html
+# capture	   :  favicon.gif	                 favicon gif
+# skip	       :  img0912.jpg.tmp
+# skip	       :  access.lock
+#* ------------------------------ >> OUT OF Course
+# https://regexone.com/problem/trimming_whitespace
+# solution > (\s+)?(.+)$ (OR) ^\s*(.*)\s*$
+# capture	:			The quick brown fox...	    The quick brown fox...
+# capture	:   jumps over the lazy dog.	        jumps over the lazy dog.
+#* ------------------------------ >> OUT OF Course
+# https://regexone.com/problem/extracting_log_data
+# solution > (\w+)\((\w+\.\w+):(\d+)\) (OR) (\w+)\(([\w+\.]+):(\d+)\)
+# skip	    :  W/dalvikvm( 1553): threadid=1: uncaught exception
+# skip	    :  E/( 1553): FATAL EXCEPTION: main
+# skip	    :  E/( 1553): java.lang.StringIndexOutOfBoundsException
+# capture	:  E/( 1553):   at widget.List.makeView(ListView.java:1727) 	makeView    ListView.java    1727
+# capture	:  E/( 1553):   at widget.List.fillDown(ListView.java:652)	    fillDown    ListView.java     652
+# capture	:  E/( 1553):   at widget.List.fillFrom(ListView.java:709)	    fillFrom    ListView.java     709
+#* ------------------------------ >> OUT OF Course
+# https://regexone.com/problem/extracting_url_data
+# solution > (\w+)://(\w+-?\w+\.?com)?(\w+)?:?(\d+)? (OR) (\w+)://([\w\-\.]+)(:(\d+))?
+# capture	:  ftp://file_server.com:21/top_secret/life_changing_plans.pdf	   ftp     file_server.com   21
+# capture	:  https://regexone.com/lesson/introduction#section	               https   regexone.com
+# capture	:  file://localhost:4040/zip_file	                               file    localhost         4040
+# capture	:  https://s3cur3-server.com:9999/	                               https   s3cur3-server.com 9999
+# capture	:  market://search/angry%20birds	                               market  search
+#? ------------------------------------- 21 -------------------------------------
+# Video 21 => https://www.youtube.com/watch?v=PJN3HzaoZ4A&list=PLBdyyeW_Z41DykncH9zzMk8T7Rm5UlZXd&index=21
+
+#                          >>>>>>>>> Strings Comparison part 1 <<<<<<<<<<
+#? ------------------------------------- 21 -------------------------------------
+# [ "$var1" = "$var2" ] >> نستخدم = بمعني هل ال string1 يطابق ال string2
+# [[ "$var1" == "$var2" ]] >> والطريقه دي تنفع برضو
+var1="Linux"
+var2="Linux"
+# if [ "$var1" = "$var2" ]
+if [[ "$var1" == "$var2" ]]
+then
+    echo "True"
+else 
+    echo "Fasle"
+fi
+#* ------------------------------ >>
+# *"Linux"* >> تستخدم هذه الطريقه للبحث عن كلمه بداخل نص 
+# *"L > معنها اي حاجه قبل الكلمه
+# x"* >> معنها اي حاجه بعد الكلمه
+# زي ال in الي في python
+var1='GUN\Linux is an OS MY PC'
+if [[ $var1 == *"Linux"* ]]
+then
+    echo "Linux in ${var1}"
+else 
+    echo "Fasle"
+fi
+#* ------------------------------ >>
+# =~ >> دي كدا طريقه استخدام ال Regular Expression
+var1='GUN\Linux is an OS MY PC'
+if [[ $var1 =~ .*Linux.* ]]
+then
+    echo "Linux in ${var1}"
+else 
+    echo "Fasle"
+fi
+#* ------------------------------ >>
+# -z >> True if string is empty.
+# -z >> تستخدم علشان تشوف هل النص فارغ ام لا
+var1=''
+if [[ -z $var1 ]]
+then
+    echo "${var1} Is Empty."
+else 
+    echo "${var1} Is Not Empty."
+fi
+#* ------------------------------ >>
+# -n >> True if string is not empty.
+# -n >> تستخدم علشان تشوف هل النص ليس فارغ ام لا
+var1='Not Empty String'
+if [[ -n $var1 ]]
+then
+    echo "${var1} Is Not Empty."
+else 
+    echo "${var1} Is Empty."
+fi
+#? ------------------------------------- 22 -------------------------------------
+# Video 22 => https://www.youtube.com/watch?v=zz4VZNaFbfA&list=PLBdyyeW_Z41DykncH9zzMk8T7Rm5UlZXd&index=22
+
+#                          >>>>>>>>> Strings Comparison part 2 <<<<<<<<<<
+#? ------------------------------------- 22 -------------------------------------
+# != >> True if the strings are not equal.
+# != >> هل ال string1 لا يساوي ال string2
+str='abc'
+patt1='a*'
+patt2='x*'
+if [[ $str != $patt2 ]] ;then
+    echo "The String ${str} Not Equal ${patt2}"
+else
+    echo "The String ${str} Equal ${patt2}"
+fi
+#* ------------------------------ >>
+#t~ عند استخدم علامه الاكبر والاصغر تتم المقارنه من خلال ال ascii code
+# https://www.ascii-code.com/
+# تتم المقارنة هنا من خلال حرف ب الحرف المقابل له علي حسب هو رقم ال DEC بتاعه في الترتيب
+# <  >> True if STRING1 sorts before STRING2 lexicographically.
+# >  >> True if STRING1 sorts after STRING2 lexicographically.
+
+var1="Linux"
+var2="Linux"
+
+if [[ $var1 > $var2 ]] ;then
+    echo "${var1} is greater Than $var2"
+elif [[ $var1 < $var2 ]] ;then
+    echo "${var1} is Less Than $var2"
+else 
+    echo "${var1} & ${var2} Equal"
+fi
+#? ------------------------------------- 23 -------------------------------------
+# Video 23 => https://www.youtube.com/watch?v=-dHhiUWttD8&list=PLBdyyeW_Z41DykncH9zzMk8T7Rm5UlZXd&index=23
+
+#                          >>>>>>>>> Case Statements part 1 <<<<<<<<<<
+#? ------------------------------------- 23 -------------------------------------
+# case $namevar in >> 
+
+echo -n "Enter the name of a country: "
+read COUNTRY
+
+echo -n "The official language of $COUNTRY is "
+
+case $COUNTRY in
+
+  Lithuania)
+    echo "Lithuanian"
+    ;;
+
+  Romania | Moldova)
+    echo "Romanian"
+    ;;
+
+  Italy | "San Marino" | Switzerland | "Vatican City")
+    echo "Italian"
+    ;;
+
+  *)
+    echo "unknown"
+    ;;
+esac
