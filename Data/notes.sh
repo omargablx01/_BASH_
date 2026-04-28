11f12f50ab072f395ae2e5ead0555447fb3b20fc
omargablx01 <o.ashraf.gabl@gmail.com>
2026-04-28 15:52:27 +0200

    edit script n

--- Data/notes.sh
+++ Data/notes.sh
@@ -1946,7 +1946,7 @@ eval echo \$$target
 calculate () {
   read -p "Enter the first number: " n1
   read -p "Enter the second number: " n2
-  echo "$n1 $1 $n2 = " $(bc -l <<< "$n1$1$n2")
+  echo "$n1 $1 $n2 = $((${n1}${1}${n2}))"
 }
 
 PS3="Select the operation: "
